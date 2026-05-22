33b6b5036c3ba2a63eec48c734b2d478c42fd2f6
gowthaaman <gokrishna98@gmail.com>
2026-05-22 13:14:17 +0530

    describe

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 import { CartProvider } from './context/CartContext.jsx'
+
+// Dynamically import CSS to prevent render-blocking CSS in initial page load
+import('./index.css');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
